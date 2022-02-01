f6787d747892797eb39ccbedbd622e73ec164101
qiyoudaoyi <qiyoudaoyi@gmail.com>
2022-02-02 05:20:07 +0800

    first commit

---
+++ src/views/CreatePost.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>Create New Post</h2>
+  <v-form>
+    
+  </v-form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+// import Post from "@/types/Post";
+
+export default defineComponent({
+  name: "CreatePost",
+  setup() {
+    return {};
+  },
+});
+</script>
